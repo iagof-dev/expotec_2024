665e08a5d6923bf6c9631227160cee3038d5db3c
Iago Fragnan <n3rdydzn@hotmail.com>
2024-04-09 10:21:07 -0300

    fix: end command

--- plugin/N3rdyTesouro/src/main/java/br/com/iagofragnan/commands/end.java
+++ plugin/N3rdyTesouro/src/main/java/br/com/iagofragnan/commands/end.java
@@ -1,1 +1,9 @@
+package br.com.iagofragnan.commands;
 
+public class end implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+      return true;
+    }
+}
+ 
