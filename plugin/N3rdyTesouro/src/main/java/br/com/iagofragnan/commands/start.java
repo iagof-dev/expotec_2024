b56987d6697c26e340d9ac24bafbc1ed5141a10c
Iago Fragnan <n3rdydzn@hotmail.com>
2024-04-09 10:23:22 -0300

    Update start.java

--- plugin/N3rdyTesouro/src/main/java/br/com/iagofragnan/commands/start.java
+++ plugin/N3rdyTesouro/src/main/java/br/com/iagofragnan/commands/start.java
@@ -11,35 +11,26 @@ public class start implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        for(Player players : Bukkit.getServer().getOnlinePlayers())
-        {
-            players.sendTitle("Ca\u00e7a ao Tesouro", ChatColor.GRAY+"Feito por Iago Fragnan", 20, 50, 20);
+        Player p = player.getObjPlayer();
+            p.sendTitle("Ca\u00e7a ao Tesouro", ChatColor.GRAY+"Feito por Iago Fragnan", 20, 50, 20);
 
             Bukkit.getScheduler().scheduleSyncDelayedTask(br.com.iagofragnan.main.getThePlugin(), new Runnable() {
                 public void run() {
-                    players.sendTitle("GITHUB", ChatColor.GRAY + "github.com/iagof-dev/", 20, 20, 20);
+                    p.sendTitle("GITHUB", ChatColor.GRAY + "github.com/iagof-dev/", 20, 20, 20);
                 }
             }, 50L);
 
             Bukkit.getScheduler().scheduleSyncDelayedTask(br.com.iagofragnan.main.getThePlugin(), new Runnable() {
                 public void run() {
-                    players.sendTitle("LinkedIn", ChatColor.GRAY+"linkedin.com/in/iago-fragnan/", 20, 20, 20);
+                    p.sendTitle("LinkedIn", ChatColor.GRAY+"linkedin.com/in/iago-fragnan/", 20, 20, 20);
                 }
             }, 210L);
 
             Bukkit.getScheduler().scheduleSyncDelayedTask(br.com.iagofragnan.main.getThePlugin(), new Runnable() {
                 public void run() {
-                    players.sendTitle("PORTFOLIO", ChatColor.GRAY+"iagofragnan.com.br", 20, 20, 20);
+                    p.sendTitle("PORTFOLIO", ChatColor.GRAY+"iagofragnan.com.br", 20, 20, 20);
                 }
             }, 270L);
-
-
-
-
-        }
-
-
-
 
 
         return true;
