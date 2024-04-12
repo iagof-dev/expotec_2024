aaf01022fc99ed558d6609e9da8a6906c449d72b
Iago Fragnan <n3rdydzn@hotmail.com>
2024-04-12 12:02:45 -0300

    fix: database fixed

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,7 @@ create database if not exists EXPOTEC_2024;
 
 use EXPOTEC_2024;
 
-create table if not exists ranking(
+create table if not exists jogadores(
     id int primary key auto_increment,
     nome varchar(64) not null,
     pontuacao int(3) not null,
